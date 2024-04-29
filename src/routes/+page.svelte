521e192b6b206c96caa12a3d766707afa4d6bd42
Yan Naung Oak <yannaungoak@gmail.com>
2024-04-29 23:38:49 +0700

    small change to test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
   import '../app.css';
 </script>
 
-<h1 class="text-3xl">Welcome to SvelteKit</h1>
+<h1 class="text-3xl">Yan's Portfolio</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
