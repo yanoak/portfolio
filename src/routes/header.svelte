dfbebdf9eafaf399ebf5a759152149988ece00fb
Yan Naung Oak <yannaungoak@gmail.com>
2024-05-08 23:52:43 +0700

    small change

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -16,6 +16,9 @@
       <a href="/contact">Contact</a>
     </li>
     <li>
+      <a href="/contact">Projects</a>
+    </li>
+    <li>
       <a href="/rss.xml" target="_blank">RSS</a>
     </li>
   </ul>
